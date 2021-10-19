72ce1ab2abd8de317c59a9069ad5965e8ea7a473
Morotalizer <Morotalizer@users.noreply.github.com>
2021-10-19 14:12:34 +0200

    Update Get Computer Last Logon For All Computers.ps1

--- Powershell/Get Computer Last Logon For All Computers.ps1
+++ Powershell/Get Computer Last Logon For All Computers.ps1
@@ -2,9 +2,9 @@
 #Get-ADComputer -Filter * -Property * | Select-Object Name,LastLogonDate,OperatingSystemVersion,ipv4Address | Export-CSV ComputerLastLogonDate_Hela_AD.csv -NoTypeInformation -Encoding UTF8
 
 # Alla i specifikt OU
-#Get-ADComputer -searchbase "OU=Windows10,OU=Arbetsstationer,OU=Datorer,DC=ragnsells,DC=net" -Filter * -Property * | Select-Object Name,LastLogonDate,OperatingSystemVersion,ipv4Address | Export-CSV Alla_Datorer_i_OU.csv -NoTypeInformation -Encoding UTF8
+#Get-ADComputer -searchbase "OU=Windows10,OU=Arbetsstationer,OU=Datorer,DC=Company,DC=net" -Filter * -Property * | Select-Object Name,LastLogonDate,OperatingSystemVersion,ipv4Address | Export-CSV Alla_Datorer_i_OU.csv -NoTypeInformation -Encoding UTF8
 
 
 # Alla inaktiva datorer i ett OU
-#Get-ADComputer -searchbase "OU=Windows10,OU=Arbetsstationer,OU=Datorer,DC=ragnsells,DC=net" -Filter {(Enabled -eq $False)} -ResultPageSize 2000 -ResultSetSize $null -Properties Name,LastLogonDate,OperatingSystemversion | Export-CSV Alla_inaktiva_Datorer_i_OU.csv -NoTypeInformation -Encoding UTF8
+#Get-ADComputer -searchbase "OU=Windows10,OU=Arbetsstationer,OU=Datorer,DC=Company,DC=net" -Filter {(Enabled -eq $False)} -ResultPageSize 2000 -ResultSetSize $null -Properties Name,LastLogonDate,OperatingSystemversion | Export-CSV Alla_inaktiva_Datorer_i_OU.csv -NoTypeInformation -Encoding UTF8
 
