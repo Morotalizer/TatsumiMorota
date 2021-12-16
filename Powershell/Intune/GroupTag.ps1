c1460d979e9d1e7d5c22e1279b95e760341d6aad
Morotalizer <Morotalizer@users.noreply.github.com>
2021-12-16 19:18:31 +0100

    Rename Powershell/GroupTag.ps1 to Powershell/Intune/GroupTag.ps1

--- Powershell/Intune/GroupTag.ps1
+++ Powershell/Intune/GroupTag.ps1
@@ -1,18 +1,26 @@
 ﻿Install-PackageProvider -Name Nuget -confirm:$false -Force
 Install-Module Microsoft.Graph.Intune -confirm:$false -Force
-Install-Module WindowsAutopilotIntune -confirm:$false -ForceImport-Module Microsoft.Graph.IntuneImport-Module WindowsAutopilotIntune
+Install-Module WindowsAutopilotIntune -confirm:$false -Force
+Import-Module Microsoft.Graph.Intune
+Import-Module WindowsAutopilotIntune
 
 $tenant = "morotalized.onmicrosoft.com"
 $authority = “https://login.windows.net/$tenant"
 $clientId = "XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXX"
 $clientSecret = "client_app_secret"
 $groupTag = "Fest"
-$requestBody=@"    {        groupTag: `"$groupTag`",    }"@
+$requestBody=
+@"
+    {
+        groupTag: `"$groupTag`",
+    }
+"@
 
 Update-MSGraphEnvironment -AppId $clientId -Quiet
 Update-MSGraphEnvironment -AuthUrl $authority -Quiet
 Connect-MSGraph -ClientSecret $ClientSecret -Quiet
-
+
+
 #$deviceSerialNumber = Get-WmiObject win32_bios | select Serialnumber
 $SerialNumber.Serialnumber = Get-WmiObject win32_bios | select Serialnumber   
 
