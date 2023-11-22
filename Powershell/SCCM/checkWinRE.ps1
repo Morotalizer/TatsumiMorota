d0b010b4b8d2e4c153a871444eb4f079a8c99e90
Morotalizer <tatsumi@morota.se>
2023-11-22 13:00:43 +0100

    winre add drviers

---
+++ Powershell/SCCM/checkWinRE.ps1
@@ -0,0 +1,13 @@
+﻿Set-ExecutionPolicy -ExecutionPolicy Bypass
+
+$DriverName = "iaStorVD.inf"
+$MountDir = "$env:SystemDrive\WinRE"
+$DriverDir = "$env:SystemDrive\DrvTemp"
+        New-Item -Path $MountDir -ItemType Directory
+  New-Item -Path $DriverDir -ItemType Directory
+
+      ReAgentC.exe /mountre /path $MountDir
+
+dism /Image:$MountDir /get-drivers
+
+dism /Image:$MountDir /remove-driver /Driver:oem0.inf
