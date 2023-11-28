1575f79b28704013c41e46d6fc75202a793398f2
Morotalizer <tatsumi@morota.se>
2023-11-28 19:33:57 +0100

    update WinRE Check

--- Powershell/SCCM/Update WinRE Storage Drivers/Verify checkWinRE.ps1
+++ Powershell/SCCM/Update WinRE Storage Drivers/Verify checkWinRE.ps1
@@ -3,11 +3,15 @@
 $DriverName = "iaStorVD.inf"
 $MountDir = "$env:SystemDrive\WinRE"
 $DriverDir = "$env:SystemDrive\DrvTemp"
-        New-Item -Path $MountDir -ItemType Directory
-  New-Item -Path $DriverDir -ItemType Directory
+New-Item -Path $MountDir -ItemType Directory
+New-Item -Path $DriverDir -ItemType Directory
 
-      ReAgentC.exe /mountre /path $MountDir
+ReAgentC.exe /mountre /path $MountDir
 
 dism /Image:$MountDir /get-drivers
 
 dism /Image:$MountDir /remove-driver /Driver:oem0.inf
+
+ReAgentc.exe /unmountre /path $MountDir /discard
+Remove-Item -Path $DriverDir -Recurse
+Remove-Item -Path $MountDir
